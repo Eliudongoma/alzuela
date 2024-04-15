a6346ef8a9d0c1c06431230d6ee7f0144e5c35bd
Eliudongoma <ongomaeliud5@gmail.com>
2024-04-15 09:07:07 +0300

    Redirect routes

--- api/startup/routes.js
+++ api/startup/routes.js
@@ -1,18 +1,18 @@
 import cors from "cors";
 import express from "express";
-// import path from "path";
+import path from "path";
 
 import auth from "../routes/auth.js";
 import users from "../routes/users.js";
 import test from "../routes/test.js";
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 export default (app) => {
-  // app.use(express.static(path.join(__dirname, "client/dist")));
-  // app.get("*", (_req, res) => {
-  //   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-  // });
+  app.use(express.static(path.join(__dirname, "client/dist")));
+  app.get("*", (_req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  });
   app.use(express.json());
   app.use(cors({ origin: "*" }));
 
