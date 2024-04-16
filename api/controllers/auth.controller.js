8acceabf9450755590c4f18289379e229225c88f
Eliudongoma <ongomaeliud5@gmail.com>
2024-04-16 20:02:34 +0300

    Signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.js"
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { name, username, email, password } = req.body;
@@ -18,6 +20,34 @@ export const signup = async (req, res, next) => {
     res.json('SignUp Successful');
   } catch (err){
    next(err)
+  }  
+}
+export const signin =async (req, res, next) => {
+  const { username, password } = req.body;    
+
+  try{
+    const validUser = await User.findOne({username});
+    if (!validUser){ 
+     return next(errorHandler(404, "Wrong credentials"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+     return next(errorHandler(404, "Wrong Credentials"));
+    }
+    // const loginDetails = new User({
+    //   username: validUser.username,
+    //   password: validUser.password
+    // })
+    // await loginDetails.sign();
+    // res.json("SignIn Successful")
+     const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+     //const { password: pass, ...rest } = validUser._doc;
+     res
+      .status(200)
+      .cookie('access_token', token, { httpOnly: true,})
+      .json("SignIn Successful");
+
+  }catch (err) {
+    next(err);
   }
-  
 }
